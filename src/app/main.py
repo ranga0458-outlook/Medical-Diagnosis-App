edd4577e3a83ad3b412d9d5c363d0ddf690aae9d
ranga0458-outlook <ranga0458@gmail.com>
2026-05-17 06:41:09 +0530

    Refactor diagnosis endpoint to process symptom input and return diagnosis results with PubMed article summaries

--- src/app/main.py
+++ src/app/main.py
@@ -1,16 +1,34 @@
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
 from functions.diagnosis_symptoms import *
 from functions.symptom_extractor import *
 from functions.pubmed_articles import *
+from functions.summarize_pubmed import *
+
+app = FastAPI()
 
 
-def main():
-    print("Hello from medical-diagnosis-app!")
+class SymptomInput(BaseModel):
+    description: str
 
-    symptoms = extract_symptoms(text="I have a headache and fever, but no nausea or fatigue")
-    get_diagnosis(symptoms)
 
-    fetch_pubmed_articles_with_metadata("back pain")
+@app.post("/diagnosis")
+def diagnosis(symptominput: SymptomInput):
+    # using regular expression to get the symptoms
+    symptoms = extract_symptoms(text=symptominput.description)
+
+    # send these symptoms to AI to get the result
+    diagnosis_result = get_diagnosis(symptoms)
+
+    # get the related articles from pub-med
+    pubmed_articles = fetch_pubmed_articles_with_metadata(" ".join(symptoms))
+
+    # summarize the articles as it contains comprehensive result
+    summary = summarize_text(pubmed_articles[:3000])
+
+    return {"symptom": symptoms, "diagnosis": diagnosis_result, "pubmed_summary": summary}
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("main:app", reload=True)
